b5e7d1f6b388b987a268c2cf3bc4d538f26856e5
axton21 <axton_n@yahoo.com>
2015-03-18 16:34:40 -0400

    oh no

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
 require 'bundler'
 Bundler.require(:default)
-
+require 'pry'
 require_relative '../models/rps_game'
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,36 @@
 class RPSGame
+class PlayTypeError < StandardError ; end
+
+	attr_reader :choice, :computer_play
+	@@rps = [:rock, :paper, :scissors]
+
+	def initialize(choice)
+		raise PlayTypeError if !@@rps.include?(choice)
+		@choice = choice
+		@computer_play = @@rps.sample
+	end
+
+	def self.valid_play?(choice)
+		if @@rps.include?(choice) then return true else return false end
+	end
+
+	def won?
+		if choice == :rock && computer_play == :scissors
+			return true
+		elseif choice == :scissors && computer_play == :paper
+			return true
+		elseif choice == :paper && computer_play == :rock
+			return true
+		else
+			return false
+		end
+	end
+
+	def tied?
+		choice == computer_play
+	end
+
+	def lost?
+		!won? && !tied?
+	end
 end
